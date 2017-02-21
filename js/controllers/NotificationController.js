02686825be87359e7dfbacdb865fae147ca4652b
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-02-21 08:15:12 -0500

    Done.

---
+++ js/controllers/NotificationController.js
@@ -0,0 +1,9 @@
+function NotificationController() {
+  ctrl = this;
+
+  ctrl.notifications = "check dat dm"
+}
+
+angular
+  .module('app')
+  .controller('NotificationController', NotificationController)
